5597b168c697c49528d2bc0a810d1c2e687d73d7
lbugasu <lbugasu@gmail.com>
2021-03-23 15:12:54 -0400

    Updating graphql endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  endpoint: 'https://ruf-coffee-api.herokuapp.com/',
+  endpoint: 'https://ruf-coffee-api.herokuapp.com',
   ws: 'wss://ruf-coffee-api.herokuapp.com',
 };
